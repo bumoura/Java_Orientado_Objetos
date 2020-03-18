373eaa1263cc6a41af38daaf4ccabb7910cecb32
bumoura <mourabrunna7@gmail.com>
2020-03-18 11:46:37 -0300

    Encapsulamento

---
+++ bytebank-encapsulado/Cliente.java
@@ -0,0 +1,32 @@
+
+public class Cliente {
+
+  private String nome;
+  private String cpf;
+  private String profisao;
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getProfisao() {
+    return profisao;
+  }
+
+  public void setProfisao(String profisao) {
+    this.profisao = profisao;
+  }
+
+}
